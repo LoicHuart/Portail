675b6be54f5fe2a547a1651e02632a5fc9878deb
GODARD <nathan.godard01@gmail.com>
2017-03-28 16:35:09 +0200

    ajout systeme rond

--- src/lib/script.js
+++ src/lib/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
-
-
+    $(".rond").hover(function(){
+        $(this).stop().animate({width: '225px' },200).css({'z-index' : '10'});
+    }, function () {
+        $(this).stop().animate({width: '17px', height: '17px' }, 200).css({'z-index' : '1'});
+    });​
 
 		
 });
