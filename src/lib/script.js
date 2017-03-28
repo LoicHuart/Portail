e50c6c6698d6beccc4fc515362d39aeed71e1a88
aimanbahouala <aimanbahouala@hotmail.fr>
2017-03-28 21:55:23 +0200

    Refonte totale FLAT DESIGN

--- src/lib/script.js
+++ src/lib/script.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
 
-    $(".item",this).hover(function(){
-        $(".rond", this).animate({right : 0});
-    }, function () {
-        $(".rond", this).animate({left : 0});
-
-    });
 
 		
 });
