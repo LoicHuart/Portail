0713ef4130d2928f9d5f301b82a807207ca3371b
BAHOUALA <aimanbahouala@hotmail.fr>
2017-04-01 10:49:18 +0200

    Ajout de la condition qui ne charge pas le javascript si l'utilisateur est sur mobile

--- src/lib/script.js
+++ src/lib/script.js
@@ -1,51 +1,58 @@
 $(document).ready(function(){
 
-    $( "#pronote" ).hover(function() {
-        $( "header" ).stop().animate({ backgroundColor: "#1abc9c" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#1abc9c" }, 500 );
-    },function() {
-        $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
-    });
 
-    $( "#cnam, #sig" ).hover(function() {
-        $( "header" ).stop().animate({ backgroundColor: "#e74c3c" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#e74c3c" }, 500 );
-    },function() {
-        $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
-    });
+    var iWindowsSize = $(window).width();
+    if (iWindowsSize >=500 ){  // SI LA TAILLE DE LA FENETRE EST SUPERIEUR A 500PX, TU EXECUTE CES SCRIPT (version PC/Tablette)
 
-    $( "#ovh" ).hover(function() {
-        $( "header" ).stop().animate({ backgroundColor: "#34495e" }, 500 );
-    },function() {
-        $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
-    });
+        $( "#pronote" ).hover(function() {
+            $( "header" ).stop().animate({ backgroundColor: "#1abc9c" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#1abc9c" }, 500 );
+        },function() {
+            $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
+        });
 
-    $( "#git" ).hover(function() {
-        $( "header" ).stop().animate({ backgroundColor: "#8e44ad" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#8e44ad" }, 500 );
-    },function() {
-        $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
-    });
+        $( "#cnam, #sig" ).hover(function() {
+            $( "header" ).stop().animate({ backgroundColor: "#e74c3c" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#e74c3c" }, 500 );
+        },function() {
+            $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
+        });
 
-    $( "#mood, #glpi" ).hover(function() {
-        $( "header" ).stop().animate({ backgroundColor: "#f1c40f" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#f1c40f" }, 500 );
-    },function() {
-        $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
-    });
+        $( "#ovh" ).hover(function() {
+            $( "header" ).stop().animate({ backgroundColor: "#34495e" }, 500 );
+        },function() {
+            $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
+        });
 
-    $( "#node" ).hover(function() {
-        $( "header" ).stop().animate({ backgroundColor: "#2ecc71" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#2ecc71" }, 500 );
-    },function() {
-        $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
-        $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
-    });
+        $( "#git" ).hover(function() {
+            $( "header" ).stop().animate({ backgroundColor: "#8e44ad" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#8e44ad" }, 500 );
+        },function() {
+            $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
+        });
 
+        $( "#mood, #glpi" ).hover(function() {
+            $( "header" ).stop().animate({ backgroundColor: "#f1c40f" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#f1c40f" }, 500 );
+        },function() {
+            $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
+        });
+
+        $( "#node" ).hover(function() {
+            $( "header" ).stop().animate({ backgroundColor: "#2ecc71" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#2ecc71" }, 500 );
+        },function() {
+            $( "header" ).stop().animate({ backgroundColor: "#424242" }, 500 );
+            $( "#lycee" ).stop().animate({ color: "#424242" }, 500 );
+        });
+
+    }else {
+
+    }
 
 
 });
