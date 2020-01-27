0ef235d48991ad725ef6e0bdf593fea311e0a365
Loic Huart <loic.huart08@gmail.com>
2020-01-27 21:11:36 +0100

    suppression d'images qui ne servent plus, modification du fichier d'import de la bdd

--- src/data/portail.sql
+++ src/data/portail.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 23 jan. 2020 à 10:22
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.3.12
+-- Client :  localhost:3306
+-- Généré le :  Lun 27 Janvier 2020 à 21:09
+-- Version du serveur :  5.7.28-0ubuntu0.18.04.4
+-- Version de PHP :  7.2.24-0ubuntu0.18.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,16 +26,14 @@ SET time_zone = "+00:00";
 -- Structure de la table `identifiants`
 --
 
-DROP TABLE IF EXISTS `identifiants`;
-CREATE TABLE IF NOT EXISTS `identifiants` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `identifiants` (
+  `id` int(50) NOT NULL,
   `login` varchar(255) NOT NULL,
-  `mdp` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `mdp` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `identifiants`
+-- Contenu de la table `identifiants`
 --
 
 INSERT INTO `identifiants` (`id`, `login`, `mdp`) VALUES
@@ -49,16 +45,15 @@ INSERT INTO `identifiants` (`id`, `login`, `mdp`) VALUES
 -- Structure de la table `portail_items`
 --
 
-DROP TABLE IF EXISTS `portail_items`;
-CREATE TABLE IF NOT EXISTS `portail_items` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `portail_items` (
+  `id` int(50) NOT NULL,
   `nom` varchar(120) CHARACTER SET utf8 NOT NULL,
   `lienhttp` varchar(120) CHARACTER SET utf8 NOT NULL,
   `cheminimage` varchar(120) CHARACTER SET utf8 NOT NULL,
   `numeroligne` int(50) NOT NULL,
   `positionInLigne` int(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+  `reverseProxy` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,15 +61,53 @@ CREATE TABLE IF NOT EXISTS `portail_items` (
 -- Structure de la table `portail_ligne`
 --
 
-DROP TABLE IF EXISTS `portail_ligne`;
-CREATE TABLE IF NOT EXISTS `portail_ligne` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `portail_ligne` (
+  `id` int(50) NOT NULL,
   `nomligne` varchar(120) CHARACTER SET utf8mb4 NOT NULL,
-  `numeroligne` int(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-COMMIT;
+  `numeroligne` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `identifiants`
+--
+ALTER TABLE `identifiants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `portail_items`
+--
+ALTER TABLE `portail_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `portail_ligne`
+--
+ALTER TABLE `portail_ligne`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `identifiants`
+--
+ALTER TABLE `identifiants`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `portail_items`
+--
+ALTER TABLE `portail_items`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+--
+-- AUTO_INCREMENT pour la table `portail_ligne`
+--
+ALTER TABLE `portail_ligne`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
